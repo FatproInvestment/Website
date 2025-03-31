88a3aa0d1b930f9480238ba30780adbcbb0cd659
FatproInvestment <FatproInvestment@users.noreply.github.com>
2025-03-31 13:10:56 +0000

    deploy: 39d5fdcf9fdb3a4184fac19c692b9e9ee57285a2

---
+++ en/assets/js/a7456010.d001d3f1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfatbro_site=self.webpackChunkfatbro_site||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
